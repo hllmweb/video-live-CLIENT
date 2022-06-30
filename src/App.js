96ef5590775c722657b47b536009f0f6255dcfb3
Hugo Mesquita <hugo.mesquita@itriad.org.br>
2022-06-30 10:24:39 -0400

    [Added] :rocket Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Home from "./pages/Home";
+import GlobalStyle from "./styles";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
